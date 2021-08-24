5e957a442ca8b6f75512d3860fa1dcd578019403
LEE Jung Hyun <akdl911215@gmail.com>
2021-08-24 23:58:09 +0900

    1

--- Doma-Shop-Project/front/src/webapp/notice/component/List/NoticeList.js
+++ Doma-Shop-Project/front/src/webapp/notice/component/List/NoticeList.js
@@ -8,7 +8,7 @@ const NoticeList = () => {
                 <h1>Notice List</h1>
                 <div className="movie-container">
                     <h2>제목</h2>
-                    <div>내용</div>
+                    <div>내용(content)</div>
                     <div className="form-wrapper">
                         <inpput className="title-input" type="text" placeholder="제목" />
                         <textarea className="text-area" placeholder="내용"></textarea>
