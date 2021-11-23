9c9cca1988a8a009fe4dd428e854af8fbb8d650a
akdl911215 <akdl911215@gmail.com>
2021-11-23 18:41:38 +0900

    1

---
+++ Doma-Shop-Project/fe/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>반갑구</p>
+    </div>
+  );
+}
+
+export default App;
--- Doma-Shop-Project/front/src/webapp/notice/component/Detail/NoticeDetail.js
+++ Doma-Shop-Project/front/src/webapp/notice/component/Detail/NoticeDetail.js
@@ -1,27 +1,9 @@
 import React from 'react';
 
 const NoticeDetail = () => {
-    if (loading) {
-        <div>loading</div>
-    } else {
+
         return (<>
-            <div className="Read">
-                <div className="list-itle">
-                    폼 게시판
-                </div>
-                <div className="read_title">
-                    {data.title}
-                </div>
-                <tbody>
-                    <tr align="center" className="table_info">
-                        <td width="15%">작성일</td>
-                        <td width="20%">{startDate}</td>
-                        <td width="15%">작성자</td>
-                        <td width="15%">링크</td>
-                    </tr>
-                </tbody>
-            </div>
+           
         </>)
-    }
-};
+    };
 export default NoticeDetail;
