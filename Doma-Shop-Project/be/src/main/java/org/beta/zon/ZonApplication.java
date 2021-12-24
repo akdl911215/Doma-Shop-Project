e574c6a18d2e3762c39a053b5ae4932e1552b6ce
akdl911215 <akdl911215@gmail.com>
2021-12-24 23:35:26 +0900

    spring boot

---
+++ Doma-Shop-Project/be/src/main/java/org/beta/zon/ZonApplication.java
@@ -0,0 +1,13 @@
+package org.beta.zon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonApplication.class, args);
+	}
+
+}
---
+++ Doma-Shop-Project/be/src/test/java/org/beta/zon/ZonApplicationTests.java
@@ -0,0 +1,14 @@
+package org.beta.zon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("contextLoad...");
+	}
+
+}
