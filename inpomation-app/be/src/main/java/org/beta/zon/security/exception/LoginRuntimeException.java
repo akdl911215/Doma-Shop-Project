e8179107e8d08a25b3dd90a0f48a2b39033e9570
akdl911215 <akdl911215@gmail.com>
2022-01-03 17:56:05 +0900

    security

--- inpomation-app/be/src/main/java/org/beta/zon/security/exception/LoginRuntimeException.java
+++ inpomation-app/be/src/main/java/org/beta/zon/security/exception/LoginRuntimeException.java
@@ -5,7 +5,7 @@ public class LoginRuntimeException extends RuntimeException {
 
     // super() : 부모 클래스로부터 상속받은 필드나 메소드를 자식 클래스에서 참조하는 데 사용하는 참조 변수
     public LoginRuntimeException() {
-        super(ErrorCode.LOGIN_FAILD.getMessage());
+        super(ErrorCode.LOGIN_FAILED.getMessage());
     }
 
     public LoginRuntimeException(String message) {
