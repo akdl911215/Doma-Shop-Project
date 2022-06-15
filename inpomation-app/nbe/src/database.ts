ac75f98e6c91cf19de83955ac12a9778a32f149d
LEEJungHyun <akdl911215@gmail.com>
2022-06-15 18:38:04 +0900

    환경설정

--- inpomation-app/nbe/src/database.ts
+++ inpomation-app/nbe/src/database.ts
@@ -4,4 +4,4 @@ import config from "./knexfile";
 const env = process.env.NODE_ENV || "development";
 
 export const knex = Knex(config[env]);
-export const knew2 = Knex(config["sf"]);s
+export const knew2 = Knex(config["sf"]);
---
+++ inpomation-app/nbe/src/knexfile.ts
@@ -0,0 +1,58 @@
+// Update with your config settings.
+
+const config = {
+    development: {
+      client: "mysql",
+      connection: {
+        host: "59.9.219.76",
+        user: "mall_sync",
+        password: "Kingdome77*",
+        database: "mall_sync",
+      },
+      pool: {
+        min: 0,
+        max: 10,
+      },
+      migrations: {
+        tableName: "knex_migrations",
+      },
+      // debug: true,
+    },
+  
+    production: {
+      client: "mysql",
+      connection: {
+        host: "59.9.219.66",
+        user: "mall_sync",
+        password: "Kingdome77*",
+        database: "mall_sync",
+      },
+      pool: {
+        min: 0,
+        max: 10,
+      },
+      migrations: {
+        tableName: "knex_migrations",
+      },
+    },
+  
+    sf: {
+      client: "mysql",
+      connection: {
+        host: "59.9.219.72",
+        user: "mall_sync",
+        password: "Kingdome77*",
+        database: "mall_sync",
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: "knex_migrations",
+      },
+    },
+  };
+  
+  export default module.exports;
+  module.exports = config;
