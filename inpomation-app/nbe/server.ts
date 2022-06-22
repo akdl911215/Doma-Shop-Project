cdb239e8f785670f722f16621516288a3ca64489
LEEJungHyun <akdl911215@gmail.com>
2022-06-22 09:23:01 +0900

    노드 서버 작업 추가

--- inpomation-app/nbe/server.ts
+++ inpomation-app/nbe/server.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -13,4 +13,14 @@ const app = express();
 
 app.listen(PORT, () => console.log(`Listening on 'http://localhost:${PORT}`));
 
-app.get("/", (req, res) => res.send('Welcome my home'))
+app.get("/", (req, res) => res.send('Welcome my home'));
+
+app.get("/users/list", (req, res) => res.send('유저 리스트'));
+app.get("/users/signup", (req, res) => res.send('회원가입'));
+app.get("/users/signin", (req, res) => res.send('로그인'));
+
+app.get("/productInfomation/register", (req, res) => res.send('상품 등록'));
+app.get("/productInfomation/modify", (req, res) => res.send('상품 수정'));
+app.get("/productInfomation/delete", (req, res) => res.send('상품 삭제'));
+
+
