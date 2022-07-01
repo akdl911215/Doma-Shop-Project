107016c3e4f6b7d805b4d634c68d07e714a5b070
LEEJungHyun <akdl911215@gmail.com>
2022-07-01 17:15:42 +0900

    공공데이타 흡연구역 api 적용 코드

--- inpomation-app/nbe/src/app.js
+++ inpomation-app/nbe/src/app.js
@@ -9,39 +9,43 @@ const FileStore = require("session-file-store")(session);
 const cookieParser = require("cookie-parser");
 const mainRouter = require("./api/routes/main/main");
 const userRouter = require("./api/routes/users/users");
+const publicRouter = require("./api/apiData/smokingAreaInGwangjinGu/smokingAreaInGwangjinGu");
 const productInfomationRouter = require("./api/routes/productInfomation/productInfomation");
 require("dotenv").config();
 const hostname = 'localhost';
 const port = 8080;
-// console.log("DB_USER : ", process.env.DB_USER); 
-// console.log("DB_PASSWORD : ", process.env.DB_PASSWORD); 
-// console.log("DB_HOST : ", process.env.DB_HOST); 
-// console.log("DB_PORT : ", process.env.DB_PORT); 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// app.use(
-//     session({
-//         secret: "keyboard cat", // 데이터를 암호화 하기 위해 필요한 옵션
-//         resave: false, // 요청이 왔을때 세션을 수정하지 않더라도 다시 저장소에 저장되도록
-//         saveUninitialized: true,  // 세션이 필요하면 세션을 실행시칸다(서버에 부담을 줄이기 위해)
-//         store : new FileStore() // 세션이 데이터를 저장하는 곳
-//     })
-// )
-
-app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 })
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(
+    session({
+        secret: "keyboard cat", // 데이터를 암호화 하기 위해 필요한 옵션
+        resave: true, // 요청이 왔을때 세션을 수정하지 않더라도 다시 저장소에 저장되도록
+        saveUninitialized: true,  // 세션이 필요하면 세션을 실행시칸다(서버에 부담을 줄이기 위해)
+        store : new FileStore() // 세션이 데이터를 저장하는 곳
+    })
+)
+
 app.use("/", mainRouter);
 // app.use("/users", userRouter);
 app.use("/productInfomation", productInfomationRouter);
-app.route('/users/register').post(userRouter.userRegister)
+app.route('/users/register').post(userRouter.userRegister);
+app.route('/users/signin').post(userRouter.userSignin);
+app.route('/users/logout').get(userRouter.userLogout);
+app.route('/publicData/smokingAreaInGwangjinGu').get(publicRouter.smokingAreaInGwangjinGu);
+
+app.get("/process/example", (req, res) => {
+    if (req.session.user) {
+        // 세션에 유저가 존재한다면
+        res.redirect("/example.html");
+    } else {
+        res.redirect("/login.html");
+    }
+})
 
 
 
-
-
-// https://webaura.tistory.com/entry/NodeJS-%EB%A1%9C%EA%B7%B8%EC%9D%B8%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85%ED%8E%B8
--- inpomation-app/nbe/src/config/knexfile.js
+++ inpomation-app/nbe/src/config/knexfile.js
@@ -1,13 +1,1 @@
 // // Update with your config settings.
-// const mysql = require("mysql2");
-
-// const db = mysql.createConnection({
-//       host: "localhost",
-//       user: "root",
-//       password: "Wjdgus@580-",
-//       database: "mall_sync",
-//       port: 3306,
-// })
-
-// db.connect(); 
-// module.exports = db;
