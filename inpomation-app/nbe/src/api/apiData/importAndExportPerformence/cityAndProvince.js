4bdeefc733d272b7580078abd6dfe13e83abb6b7
akdl911215 <akdl911215@gmail.com>
2022-07-09 15:52:52 +0900

    be public data try catcg control

--- inpomation-app/nbe/src/api/apiData/importAndExportPerformence/cityAndProvince.js
+++ inpomation-app/nbe/src/api/apiData/importAndExportPerformence/cityAndProvince.js
@@ -1,3 +1,4 @@
+const { throws } = require("assert");
 const request = require("request");
 const parseString = require("xml2js").parseString;
 require("dotenv").config();
@@ -26,39 +27,54 @@ exports.cityAndProvince = (req, res) => {
       console.log(`시도별 수출입실적 조회 ERROR : ${err}`);
       throw err;
     }
+    try {
+      parseString(body, (err, result) => {
+        console.log("parseString body : ", body);
+        console.log("parseString result : ", result);
 
-    parseString(body, (err, result) => {
-      console.log("parseString body : ", body);
-      console.log("parseString result : ", result);
+        if (result.response.body[0] === "") {
+          console.error("11 request pasing error : ", err);
+        }
 
-      if (err) {
-        console.error(`시도별 수출입실적 조회 parseSring error : ${err}`);
+        if (err) {
+          console.error(`시도별 수출입실적 조회 parseSring error : ${err}`);
+          res.json({
+            result: {
+              errorMsg: result,
+            },
+          });
+          return;
+        }
+
         res.json({
           result: {
-            errorMsg: result,
+            resultMsg: result.response.header[0].resultMsg[0],
+            cmtrBlncAmt:
+              result.response.body[0].items[0].item[0].cmtrBlncAmt[0], // cmtrBlncAmt : 무역지수
+            expCnt: result.response.body[0].items[0].item[0].expCnt[0], // expCnt : 수출건수
+            expUsdAmt: result.response.body[0].items[0].item[0].expUsdAmt[0], // expUsdAmt : 수출금액
+            impCnt: result.response.body[0].items[0].item[0].impCnt[0], // impCnt : 수입건수
+            impUsdAmt: result.response.body[0].items[0].item[0].impUsdAmt[0], // impUsdAmt : 수입금액
+            priodTitle: result.response.body[0].items[0].item[0].priodTitle[0], // priodTitle : 기간
+            cmtrBlncAmt2:
+              result.response.body[0].items[0].item[1].cmtrBlncAmt[0], // cmtrBlncAmt : 무역지수
+            expCnt2: result.response.body[0].items[0].item[1].expCnt[0], // expCnt : 수출건수
+            expUsdAmt2: result.response.body[0].items[0].item[1].expUsdAmt[0], // expUsdAmt : 수출금액
+            impCnt2: result.response.body[0].items[0].item[1].impCnt[0], // impCnt : 수입건수
+            impUsdAmt2: result.response.body[0].items[0].item[1].impUsdAmt[0], // impUsdAmt : 수입금액
+            priodTitle2: result.response.body[0].items[0].item[1].priodTitle[0], // priodTitle : 기간
+            sidoNm2: result.response.body[0].items[0].item[1].sidoNm[0], // sidoNm : 광역시
           },
         });
-        return;
-      }
-
+      });
+    } catch (err) {
       res.json({
         result: {
           resultMsg: result.response.header[0].resultMsg[0],
-          cmtrBlncAmt: result.response.body[0].items[0].item[0].cmtrBlncAmt[0], // cmtrBlncAmt : 무역지수
-          expCnt: result.response.body[0].items[0].item[0].expCnt[0], // expCnt : 수출건수
-          expUsdAmt: result.response.body[0].items[0].item[0].expUsdAmt[0], // expUsdAmt : 수출금액
-          impCnt: result.response.body[0].items[0].item[0].impCnt[0], // impCnt : 수입건수
-          impUsdAmt: result.response.body[0].items[0].item[0].impUsdAmt[0], // impUsdAmt : 수입금액
-          priodTitle: result.response.body[0].items[0].item[0].priodTitle[0], // priodTitle : 기간
-          cmtrBlncAmt2: result.response.body[0].items[0].item[1].cmtrBlncAmt[0], // cmtrBlncAmt : 무역지수
-          expCnt2: result.response.body[0].items[0].item[1].expCnt[0], // expCnt : 수출건수
-          expUsdAmt2: result.response.body[0].items[0].item[1].expUsdAmt[0], // expUsdAmt : 수출금액
-          impCnt2: result.response.body[0].items[0].item[1].impCnt[0], // impCnt : 수입건수
-          impUsdAmt2: result.response.body[0].items[0].item[1].impUsdAmt[0], // impUsdAmt : 수입금액
-          priodTitle2: result.response.body[0].items[0].item[1].priodTitle[0], // priodTitle : 기간
-          sidoNm2: result.response.body[0].items[0].item[1].sidoNm[0], // sidoNm : 광역시
         },
       });
-    });
+      console.error("request pasing error : ", err);
+      throw new Error("request pasing error : ", err);
+    }
   });
 };
