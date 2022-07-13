23e73a53071674090d746e9759043beb50b65246
akdl911215 <akdl911215@gmail.com>
2022-07-13 11:27:22 +0900

    be knex 구상중

---
+++ inpomation-app/nbe/src/config/database copy 2.js
@@ -0,0 +1,11 @@
+// // import Knex from "knex";
+// const Knew = require("knex");
+// // import config from "./knexfile";
+// const config = require("./knexfile");
+
+// console.log("process.env.DB_HOST : ", process.env.DB_HOST);
+// console.log("pprocess.env.NODE_ENV : ", process.env.NODE_ENV);
+// const env = process.env.NODE_ENV || process.env.DB_HOST;
+// console.log("env :: ", env);
+
+// export const knex = Knex(config[env]);
--- inpomation-app/nbe/src/config/knexfile.js
+++ inpomation-app/nbe/src/config/knexfile.js
@@ -1,1 +1,42 @@
-// // Update with your config settings.
+// // // Update with your config settings.
+// require("dotenv").config();
+// // host: process.env.DB_HOST,
+// // user: process.env.DB_USER,
+// // password: process.env.DB_PASSWORD,
+// // database: process.env.DB_DATABASES,
+// // port: process.env.DB_PORT,
+// const config = {
+//   development: {
+//     client: "mysql",
+//     connection: {
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_DATABASES,
+//     },
+//     pool: {
+//       min: 0,
+//       max: 10,
+//     },
+//     // migrations: {
+//     //   tableName: "",
+//     // },
+//   },
+
+//   production: {
+//     client: "mysql",
+//     connection: {
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_DATABASES,
+//     },
+//     pool: {
+//       min: 0,
+//       max: 10,
+//     },
+//   },
+// };
+
+// export default module.exports;
+// module.exports = config;
