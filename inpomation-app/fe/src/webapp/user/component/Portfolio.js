884795fd19c74989e237f2f761cc2f3c3f47ba2c
akdl911215 <akdl911215@gmail.com>
2022-07-18 21:59:56 +0900

    fe chart 적용

--- inpomation-app/fe/src/webapp/user/component/Portfolio.js
+++ inpomation-app/fe/src/webapp/user/component/Portfolio.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { RadialChart } from "react-vis";
 
 const Portfolio = () => {
-  return <>포트폴리오</>;
+  const myData = [{ angle: 122 }, { angle: 120 }, { angle: 22 }];
+  return (
+    <>
+      <RadialChart data={myData} width={300} height={300} />
+    </>
+  );
 };
 export default Portfolio;
