f3fdaec4572a9bb4e1e2ebc40b7ea82421040cf4
akdl911215 <akdl911215@naver.com>
2022-07-19 18:01:47 +0900

    be 데이터베이스 변경 내용 추가

--- inpomation-app/nbe/src/api/routes/users/users.js
+++ inpomation-app/nbe/src/api/routes/users/users.js
@@ -1,5 +1,27 @@
 const db = require("../../middlewares/pool");
 const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+console.log("JWT_SECRET : ", process.env.JWT_SECRET);
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      // 유효기간 초과
+      return res.status(419).json({
+        code: 419,
+        message: "토큰이 만료되었습니다",
+      });
+    }
+
+    return res.status(401).json({
+      code: 401,
+      message: "유효하지 않은 토큰입니다.",
+    });
+  }
+};
 
 exports.userLogout = (req, res) => {
   console.log("로그아웃");
@@ -41,22 +63,40 @@ exports.userSignin = (req, res) => {
                     }
                     const hour = 3600000;
                     if (rows.length) {
-                      req.session.username = rows[0].username;
-                      // req.session.password = rows[0].password;
-                      req.session.isLogined = true;
-                      req.session.cookie.expires = new Date(Date.now() + hour);
-                      req.session.cookie.maxAge = hour;
-                      req.session.save(() => {
-                        res.json({
-                          result: "로그인 성공",
-                        });
-                        // res.redirect("/");
-                      });
-                      console.log("req.session : ", req.session);
+                      // req.session.username = rows[0].username;
+                      // // req.session.password = rows[0].password;
+                      // req.session.isLogined = true;
+                      // req.session.cookie.expires = new Date(Date.now() + hour);
+                      // req.session.cookie.maxAge = hour;
 
-                      for (let key in req.session) {
-                        console.log("attr : ", key, ", value : ", req.session);
-                      }
+                      const token = jwt.sign(
+                        {
+                          type: "JWT",
+                          username: username,
+                        },
+                        process.env.JWT_SECRET,
+                        {
+                          expiresIn: "1m",
+                          issuer: "토큰 발급자",
+                        }
+                      );
+
+                      res.status(200).json({
+                        code: 200,
+                        message: "토큰이 발급되었습니다.",
+                        token: token,
+                      });
+                      // req.session.save(() => {
+                      //   res.json({
+                      //     result: "로그인 성공",
+                      //   });
+                      //   // res.redirect("/");
+                      // });
+                      // console.log("req.session : ", req.session);
+
+                      // for (let key in req.session) {
+                      //   console.log("attr : ", key, ", value : ", req.session);
+                      // }
                     }
                   });
                 } else {
