d71a57b10af17245f47f1fb38733bf0cf109ff29
akdl911215 <akdl911215@naver.com>
2022-07-31 16:40:57 +0900

    fe 로그아웃 버튼 로직 변경

--- inpomation-app/fe/src/webapp/common/component/SignOutButton.js
+++ inpomation-app/fe/src/webapp/common/component/SignOutButton.js
@@ -1,19 +1,18 @@
 import React from "react";
-// import { useNavigate } from "react-router";
 import { Button } from "semantic-ui-react";
 
 const SignOutButton = () => {
-  // const navigate = useNavigate();
-  const clickButton = () => {
-    alert("로그아웃을 진행합니다");
-    localStorage.removeItem("jwtToken");
-    localStorage.removeItem("roles");
-    window.location.reload();
-  };
-
   return (
     <>
-      <Button secondary onClick={clickButton}>
+      <Button
+        secondary
+        onClick={() => {
+          alert("로그아웃을 진행합니다");
+          sessionStorage.removeItem("jwtToken");
+          sessionStorage.removeItem("roles");
+          window.location.reload();
+        }}
+      >
         로그아웃
       </Button>
     </>
