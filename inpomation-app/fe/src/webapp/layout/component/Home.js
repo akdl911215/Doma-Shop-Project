79fb4d15f3ad8079a3f6dbd5d0a07ba8a7bd0cf9
akdl911215 <akdl911215@naver.com>
2022-08-01 21:49:04 +0900

    fe 유튜브 영상 업로드 작업 추가

--- inpomation-app/fe/src/webapp/layout/component/Home.js
+++ inpomation-app/fe/src/webapp/layout/component/Home.js
@@ -3,11 +3,13 @@ import "webapp/layout/style/Home.css";
 import InvestingInfomationList from "../../investing-infomation/component/InvestingInfomationList";
 import Header from "./Header";
 import Footer from "./Footer";
+import YoutubeRegister from "webapp/youtube-upload/component/YoutubeRegister";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <YoutubeRegister />
       <InvestingInfomationList />
       <Footer />
     </>
