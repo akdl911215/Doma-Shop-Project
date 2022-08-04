2119ce269e8059bad6cf424533521499c0b5b7a4
akdl911215 <akdl911215@naver.com>
2022-08-04 21:53:47 +0900

    fe 포트폴리오 작업

--- inpomation-app/fe/src/webapp/user/user-portfolio-component/ProtfolioCashAsset.js
+++ inpomation-app/fe/src/webapp/user/user-portfolio-component/ProtfolioCashAsset.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { RadialChart } from "react-vis";
 import { Form, Button } from "semantic-ui-react";
+import styles from "../user-portfolio-style/ProtfolioCashAsset.module.css";
 
 const ProtfolioCashAsset = () => {
   // 포트폴리오 비율 현금 vs 자산
@@ -10,8 +11,16 @@ const ProtfolioCashAsset = () => {
   });
   const [options, setOptions] = useState("cash");
   const cashVsAssetRatio = [
-    { angle: cashAsset.cash },
-    { angle: cashAsset.asset },
+    {
+      angle: Number(cashAsset.cash),
+      label: cashAsset.cash === 0 ? "" : "현금",
+      subLabel: cashAsset.cash === 0 ? "" : cashAsset.cash,
+    },
+    {
+      angle: Number(cashAsset.asset),
+      label: cashAsset.asset === 0 ? "" : "자산",
+      subLabel: cashAsset.asset === 0 ? "" : cashAsset.asset,
+    },
   ];
   const handleChange = (e) => {
     const { value } = e.target;
@@ -23,17 +32,84 @@ const ProtfolioCashAsset = () => {
 
   return (
     <>
-      <RadialChart data={cashVsAssetRatio} width={300} height={300} />
-      <select name="cashAsset" onChange={(e) => setOptions(e.target.value)}>
-        <option value="cash" selected="selected">
-          현금
-        </option>
-        <option value="asset">자산</option>
-      </select>
-      <Form size="small">
-        <Form.Input fluid name="cash" onChange={handleChange} />
-        <Button type="submit">Submit</Button>
-      </Form>
+      <div className={styles.box}>
+        {/* 차트 */}
+        <RadialChart
+          data={cashVsAssetRatio}
+          showLabels={true}
+          width={500}
+          height={500}
+          radius={130}
+        />
+
+        {/* 디스플레이 인풋 */}
+        <div>
+          <div className={styles.displayBox}>
+            <Form size="small">
+              <Form.Input
+                fluid
+                value="현금"
+                className={styles.displayInputBoxName}
+                readOnly
+              />
+            </Form>
+          </div>
+          <div className={styles.displayBox}>
+            <Form size="small">
+              <Form.Input
+                fluid
+                value={cashAsset.cash}
+                className={styles.displayInputBoxValue}
+                readOnly
+              />
+            </Form>
+          </div>
+        </div>
+
+        <div>
+          <div className={styles.displayBox}>
+            <Form size="small">
+              <Form.Input
+                fluid
+                value="자산"
+                className={styles.displayInputBoxName}
+                readOnly
+              />
+            </Form>
+          </div>
+          <div className={styles.displayBox}>
+            <Form size="small">
+              <Form.Input
+                fluid
+                value={cashAsset.asset}
+                className={styles.displayInputBoxValue}
+                readOnly
+              />
+            </Form>
+          </div>
+        </div>
+
+        {/* 셀렉트 인풋 서브밋 */}
+        <div className={styles.cashAssetBox}>
+          <div className={styles.floatBox}>
+            <select
+              name="cashAsset"
+              onChange={(e) => setOptions(e.target.value)}
+            >
+              <option value="cash">현금</option>
+              <option value="asset">자산</option>
+            </select>
+          </div>
+          <div className={styles.inputBox}>
+            <Form size="small">
+              <Form.Input fluid name="cash" onChange={handleChange} />
+            </Form>
+          </div>
+          <div className={styles.floatBox}>
+            <Button type="submit">Submit</Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
