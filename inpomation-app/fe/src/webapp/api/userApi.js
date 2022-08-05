143516401e2947ad01afad697fbff12106efedc8
akdl911215 <akdl911215@naver.com>
2022-08-05 18:20:41 +0900

    코드 정리

--- inpomation-app/fe/src/webapp/api/userApi.js
+++ inpomation-app/fe/src/webapp/api/userApi.js
@@ -18,21 +18,13 @@ export const UserSignupDataAPI = (states) =>
 export const UserSigninDataAPI = (states) =>
   client.post(backUrl + "/users/signin", states);
 
-export const UserModifyDataAPI = (user) => {
-  console.log("modify 실행!!");
+export const UserModifyDataAPI = (user) =>
+  client.post(backUrl + "/users/modify", user);
 
-  return client.post(backUrl + "/users/modify", user);
-};
-
-export const UserInquiryDataAPI = (user) => {
-  console.log("inquiry 실행");
-
-  return client.post(backUrl + "/users/inquiry", { username: user });
-};
+export const UserInquiryDataAPI = (user) =>
+  client.post(backUrl + "/users/inquiry", { username: user });
 
 export const UserAuthDataAPI = (token, roles) => {
-  console.log("!!!! ", token, "/ ", roles, "/ ");
-
   client.defaults.headers.common["Authorization"] = "Bearer " + token;
   return client.post(backUrl + `/users/auth`, {
     roles: roles,
