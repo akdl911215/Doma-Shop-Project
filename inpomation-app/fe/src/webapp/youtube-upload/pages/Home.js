f03a2d51442c4cc3056610df3b87002efa151fa0
akdl911215 <akdl911215@naver.com>
2022-08-20 18:34:58 +0900

    fe 유트브 적용 작업 추가

--- inpomation-app/fe/src/webapp/youtube-upload/pages/Home.js
+++ inpomation-app/fe/src/webapp/youtube-upload/pages/Home.js
@@ -3,10 +3,10 @@ import youtubeData from "../data/youtubeData.json";
 import HomeFilter from "../component/home/HomeFilter";
 import HomeCard from "../component/home/HomeCard";
 import { useState } from "react";
-
-const target = ["전체", "투자", "방법", "시황"];
+const target = ["홈", "전체"];
+// const target = ["전체", "투자", "방법", "시황"];
 const Home = () => {
-  const [filter, setFilter] = useState("전체");
+  const [filter, setFilter] = useState("홈");
 
   const mapFunc = (data, index) => {
     return (
@@ -20,7 +20,7 @@ const Home = () => {
   };
 
   const filterFunc = (data) => {
-    if (filter === "전체" || data.category === filter) return true;
+    if (filter === "홈" || data.category === filter) return true;
     return false;
   };
 
