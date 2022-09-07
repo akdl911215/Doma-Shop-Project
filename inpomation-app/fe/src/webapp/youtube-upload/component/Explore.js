84b0a034e14da1761ac4dc3d1b9772333c63fe6f
akdl911215 <akdl911215@naver.com>
2022-09-07 14:56:02 +0900

    fe explore 개선 작업 추가

--- inpomation-app/fe/src/webapp/youtube-upload/component/Explore.js
+++ inpomation-app/fe/src/webapp/youtube-upload/component/Explore.js
@@ -23,8 +23,8 @@ const Explore = () => {
   const { searchList } = useSelector(({ YoutubeReducer }) => ({
     searchList: YoutubeReducer?.YoutubeSearchListInitial,
   }));
-  console.log("searchList : ", searchList);
-  console.log("searchList.length : ", searchList.length);
+  console.log("Explore searchList : ", searchList);
+  console.log("Explore searchList.length : ", searchList.length);
 
   return (
     <>
@@ -35,8 +35,8 @@ const Explore = () => {
       <div className={styles.contentsDiv}>
         <Menu />
         <ContentsLayout>
-          {searchList.length === 0
-            ? null
+          {searchList?.length === 0
+            ? ""
             : searchList?.map((data, index) => {
                 return (
                   <ExploreCard key={`explore-card-${index}`} data={data} />
