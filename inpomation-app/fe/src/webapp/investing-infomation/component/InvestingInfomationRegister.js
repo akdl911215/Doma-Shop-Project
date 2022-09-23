d9f6cd3cfc62b52f524bace02e27b85b4b8c9c2e
akdl911215 <akdl911215@naver.com>
2022-09-23 18:23:02 +0900

    fe invest board register work add

--- inpomation-app/fe/src/webapp/investing-infomation/component/InvestingInfomationRegister.js
+++ inpomation-app/fe/src/webapp/investing-infomation/component/InvestingInfomationRegister.js
@@ -14,27 +14,36 @@ import GoHomeButton from "webapp/common/component/GoHomeButton";
 import { useNavigate } from "react-router-dom";
 import { client } from "webapp/api/Client";
 import styles from "../style/InvestingInfomationRegister.module.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "../style/InvestingInfomationRegister.css";
+import { useEffect } from "react";
+import { useCallback } from "react";
 
 const InvestingInfomationRegister = () => {
   const [selectedFile, setSelectedFile] = useState([]);
   const navigate = useNavigate();
 
-  const addImage = (event) => {
-    const nowSelectImageList = event.target.files;
-    const nowImageURLList = [...selectedFile];
-    for (let i = 0; i < nowSelectImageList.length; i++) {
-      const nowImageUrl = URL.createObjectURL(nowSelectImageList[i]);
-      nowImageURLList.push(nowImageUrl);
-    }
-    setSelectedFile(nowImageURLList);
-  };
+  // const addImage = (event) => {
+  //   const nowSelectImageList = event.target.files;
+  //   const nowImageURLList = [...selectedFile];
+  //   for (let i = 0; i < nowSelectImageList.length; i++) {
+  //     const nowImageUrl = URL.createObjectURL(nowSelectImageList[i]);
+  //     nowImageURLList.push(nowImageUrl);
+  //   }
+  //   setSelectedFile(nowImageURLList);
+  // };
 
   const [register, setRegister] = useState({
     title: "",
-    writer: "",
+    writer:
+      sessionStorage.getItem("username") === null
+        ? ""
+        : sessionStorage.getItem("username"),
     content: "",
-    viewCount: "",
+    viewCount: 0,
   });
+  useEffect(() => console.log("register : ", register), [register]);
 
   const handleFileUpload = () => {
     // const formData = new FormData();
@@ -59,9 +68,16 @@ const InvestingInfomationRegister = () => {
     // navigate("/product_infomation_list");
   };
 
-  // https://okky.kr/events/new
-  // https://kr.investing.com/
-  // https://falaner.tistory.com/59
+  const handleChange = useCallback((e) => {
+    const { name, value } = e.target;
+    console.log(`name: ${name}, value: ${value}`);
+
+    setRegister({
+      ...register,
+      [name]: value,
+    });
+  });
+
   return (
     <>
       <div className={styles.board}>
@@ -71,11 +87,38 @@ const InvestingInfomationRegister = () => {
             <input
               placeholder="제목을 입력해주세요."
               className={styles.titleInput}
+              name="title"
+              onChange={handleChange}
             />
           </div>
           <div className={styles.contentBox}>
             <span>본문</span>
-            <input className={styles.contentInput} />
+            {/* <textarea className={styles.contentInput} /> */}
+            <CKEditor
+              editor={ClassicEditor}
+              // data="<p>Hello from CKEditor 5!</p>"
+              config={{
+                placeholder: "내용을 입력하세요.",
+              }}
+              onReady={(editor) => {
+                // You can store the "editor" and use when it is needed.
+                console.log("Editor is ready to use!", editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+                setRegister({
+                  ...register,
+                  content: data.replace(/(<([^>]+)>)/gi, ""),
+                });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />
           </div>
           <div className={styles.btnBox}>
             <button className={styles.uploadBtn}>
@@ -89,7 +132,11 @@ const InvestingInfomationRegister = () => {
             <button className={styles.cancelBtn}>
               <span
                 className={styles.cancelText}
-                onClick={() => navigate("/investing_infomation_list")}
+                onClick={() => {
+                  if (window.confirm("작성을 취소하겠습니까?")) {
+                    navigate("/investing_infomation_list");
+                  }
+                }}
               >
                 취소
               </span>
