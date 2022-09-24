77ec0aa323071941855d273748d64cc525435097
akdl911215 <akdl911215@naver.com>
2022-09-24 22:05:55 +0900

    be invest board register work add

--- inpomation-app/nbe/src/investing/investingController.js
+++ inpomation-app/nbe/src/investing/investingController.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const service = require("./investingService");
 
-router.post("/register", async (req, res, next) => {
-  const result = await service.register(req?.body);
-  console.log("result : ", result);
-
-  res.json(result);
-});
+// regdate modifydate deletedate 추가하기
+router.post("/register", async (req, res, next) =>
+  res.json(await service.register(req?.body))
+);
 
 router.post("/list", async (req, res, next) => {
   const result = await service.list(req?.body);
--- inpomation-app/nbe/src/investing/investingRepository.js
+++ inpomation-app/nbe/src/investing/investingRepository.js
@@ -3,12 +3,30 @@ const db = require("../api/middlewares/pool");
 exports.register = async (req, res, next) => {
   console.log("req : ", req);
   const { userId, writer, title, content } = req;
-  const sql = `INSERT INTO invesing_board(user_id, writer, title, content)
-                VALUES ('${userId}', '${writer}', ${title}, '${content}')`;
+  const sql = `INSERT INTO investing_board (user_id, writer, title, content) VALUES (${userId}, '${writer}', '${title}', '${content}')`;
   console.log("sql : ", sql);
   return new Promise((resolve, reject) => {
     try {
-      //
+      db.getConnectionPool((connection) => {
+        connection.query(sql, (err, doc) => {
+          if (err) {
+            console.error("connection error : ", err);
+            resolve({
+              message: "투자 게시판 업로드 실패",
+              error: err,
+            });
+          }
+
+          if (doc) {
+            console.log("connection result : ", doc);
+            resolve({
+              code: 200,
+              message: "투자 게시판 업로드 성공",
+            });
+          }
+        });
+        connection.release();
+      });
     } catch (err) {
       console.error("register promise error : ", err);
     }
