70e9bfaf9075aee03a07012f5e9ceb5ed6882298
akdl911215 <akdl911215@naver.com>
2022-09-26 17:21:20 +0900

    node db datetime format

--- inpomation-app/nbe/src/config/database.js
+++ inpomation-app/nbe/src/config/database.js
@@ -6,6 +6,7 @@ const dbConfig = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASES,
   port: process.env.DB_PORT,
+  dateStrings: "date",
 };
 
 module.exports = dbConfig;
