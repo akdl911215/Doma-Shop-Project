b924a4c885eed22fbd798c8bfb8511bff12b2315
akdl911215 <akdl911215@naver.com>
2022-10-12 10:20:51 +0900

    fe signup email check work add

--- inpomation-app/fe/src/webapp/user/component/Signup.js
+++ inpomation-app/fe/src/webapp/user/component/Signup.js
@@ -9,10 +9,17 @@ const Signup = () => {
   const [signup, setSignup] = useState({
     username: "",
     password: "",
+    confirmPassword: "",
     name: "",
     address: "",
     email: "",
     phoneNumber: "",
+  });
+
+  const [emailAuthBool, setEmailAuthBool] = useState({
+    bool: false,
+    number: 0,
+    emailCheck: 0,
   });
   const navigate = useNavigate();
 
@@ -25,7 +32,6 @@ const Signup = () => {
         ? (input = value.replace(/[^0-9]/g, ""))
         : (input = value);
 
-      console.log(`name : ${name}, value : ${input}`);
       setSignup({
         ...signup,
         [name]: input,
@@ -49,40 +55,24 @@ const Signup = () => {
     }
   };
 
-  const [emailAuthBool, setEmailAuthBool] = useState({
-    bool: false,
-    number: 0,
-    emailCheck: 0,
-  });
   const emailAuth = () => {
     // emailAuthBool true 면 이메일 인증 완료
     UserEmailAuthDataAPI({
       email: signup.email,
     })
       .then((res) => {
-        console.log("res : ", res);
         if (res?.data?.code === 200) {
-          if (res?.data?.code === 200) {
-            setEmailAuthBool({
-              ...emailAuthBool,
-              number: res?.data?.number,
-            });
-          }
+          setEmailAuthBool({
+            ...emailAuthBool,
+            number: res?.data?.number,
+          });
         }
       })
       .catch((err) => console.error("email auth error : ", err));
   };
 
   const emailCheck = () => {
-    console.log(
-      `emailAuthBool.number  : ${emailAuthBool.number}, emailAuthBool.emailCheck : ${emailAuthBool.emailCheck}`
-    );
-    console.log(
-      "emailAuthBool.number === emailAuthBool.emailCheck : ",
-      emailAuthBool.number === emailAuthBool.emailCheck
-    );
-
-    if (emailAuthBool.number === emailAuthBool.emailCheck) {
+    if (emailAuthBool.number === Number(emailAuthBool.emailCheck)) {
       setEmailAuthBool({
         ...emailAuthBool,
         bool: true,
@@ -94,7 +84,6 @@ const Signup = () => {
     (e) => {
       const { name, value } = e.target;
 
-      console.log(`name : ${name}, value : ${value}`);
       setEmailAuthBool({
         ...emailAuthBool,
         [name]: value,
@@ -102,6 +91,28 @@ const Signup = () => {
     },
     [emailAuthBool]
   );
+
+  const goSignup = () => {
+    if (signup.username === "") {
+      alert("아이디를 입력하세요.");
+    } else if (signup.password === "") {
+      alert("비밀번호를 입력하세요.");
+    } else if (signup.confirmPassword === "") {
+      alert("비밀번호 확인을 입력하세요.");
+    } else if (signup.password !== signup.confirmPassword) {
+      alert("비밀번호와 비밀번호 확인의 값이 다릅니다.");
+    } else if (signup.name === "") {
+      alert("이름을 입력하세요.");
+    } else if (signup.address === "") {
+      alert("주소를 입력하세요.");
+    } else if (signup.email === "") {
+      alert("이메일을 입력하세요.");
+    } else if (emailAuthBool.bool === false) {
+      alert("이메일 인증이 필요합니다.");
+    } else if (signup.phoneNumber === "") {
+      alert("핸드폰 번호를 입력하세요.");
+    }
+  };
 
   return (
     <>
@@ -121,6 +132,14 @@ const Signup = () => {
           <input
             name="password"
             type="password"
+            placeholder="비밀번호를 입력하세요"
+            className={styles.signupInput}
+            onChange={handleChange}
+          ></input>
+          <span>비밀번호 확인</span>
+          <input
+            name="confirmPassword"
+            type="confirmPassword"
             placeholder="비밀번호를 입력하세요"
             className={styles.signupInput}
             onChange={handleChange}
@@ -149,7 +168,7 @@ const Signup = () => {
           {!emailAuthBool.bool ? (
             <button onClick={emailAuth}>이메일 인증</button>
           ) : (
-            <button disabled={true}>인증 완료</button>
+            <button disabled={emailAuthBool.bool}>인증 완료</button>
           )}
           <span>E-mail 확인</span>
           <input
@@ -157,8 +176,11 @@ const Signup = () => {
             placeholder="인증번호를 입력하세요"
             className={styles.signupInput}
             onChange={emailHandleChange}
+            disabled={emailAuthBool.bool}
           ></input>
-          <button onClick={emailCheck}>인증번호 확인</button>
+          <button onClick={emailCheck} disabled={emailAuthBool.bool}>
+            인증번호 확인
+          </button>
           <span>핸드폰 번호</span>
           <input
             name="phoneNumber"
@@ -171,7 +193,7 @@ const Signup = () => {
               <Form.Field
                 secondary
                 control={Button}
-                onClick={emailAuthBool === false ? null : handleSubmit}
+                onClick={emailAuthBool.bool === false ? goSignup : handleSubmit}
               >
                 회원가입
               </Form.Field>
