90d6ed5ac14cf5c2ec598df4d7517096c81e3599
akdl911215 <akdl911215@naver.com>
2023-03-27 19:51:07 +0900

    creditors work

--- loan/nestjs-back/src/creditors/application/usecase/creditors.create.use.case.spec.ts
+++ loan/nestjs-back/src/creditors/application/usecase/creditors.create.use.case.spec.ts
@@ -4,12 +4,14 @@ import { ConfigService } from "@nestjs/config";
 import { CreditorsCreateUseCase } from "./creditors.create.use.case";
 import { PrismaService } from "../../../_common/infrastructures/prisma/prisma.service";
 import { CreditorsCreateRepository } from "../../infrastructure/repository/creditors.create.repository";
-import { UsersExistsUserInterfaceInputDto } from "../../../loans/inbound/dtos/interfaces/users.exists.user.interface.input.dto";
 import { UsersExistsUniqueIdRepository } from "../../infrastructure/repository/users.exists.unique.id.repository";
 import { CreditorsCreateAdaptorInputDto } from "../../inbound/dtos/adaptors/creditors.create.adaptor.input.dto";
+import { v4 as uuidv4 } from "uuid";
 
 describe("level create useCase", () => {
   let service: CreditorsCreateUseCase;
+  let prisma: PrismaService;
+  let UUID: uuidv4;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -29,6 +31,39 @@ describe("level create useCase", () => {
     }).compile();
 
     service = module.get<CreditorsCreateUseCase>(CreditorsCreateUseCase);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  describe("mocking", () => {
+    const DATE = new Date();
+    const dto = {
+      id: uuidv4,
+      creditorsUniqueId: "",
+      creditorsConfirmationId: Date.now(),
+      createdAt: DATE,
+      updatedAt: DATE,
+      deletedAt: null,
+    };
+
+    it("should success mock broadcast version the mocking", async () => {
+      const mock = jest
+        .spyOn(prisma.creditors, "create")
+        .mockResolvedValue(dto);
+      await service.create(dto);
+      expect(mock).toHaveBeenCalledWith({
+        data: dto,
+      });
+    });
+
+    it("should find one by id what spec broadcast", async () => {
+      const mock = jest
+        .spyOn(prisma.stream, "findFirst")
+        .mockResolvedValue(dto);
+      await service.get(dto);
+      expect(mock).toHaveBeenCalledWith({
+        where: { id: dto.id, deletedAt: null },
+      });
+    });
   });
 
   let dto: CreditorsCreateAdaptorInputDto;
